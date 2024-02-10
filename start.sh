eeb17db3ee14cd295bda4ee1d671ad35837795fb
Michael Kazakov <mike.kazakov@gmail.com>
2024-02-10 19:16:31 +0000

    Setting up

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set +x
+set -e
+set -o pipefail
+
 docker build --tag nc_webdav_ubuntu_2004 webdav_ubuntu_2004
 docker create --name nc_webdav_ubuntu_2004 -p 9080:80 nc_webdav_ubuntu_2004
 docker start nc_webdav_ubuntu_2004
