9eb64e08e3fc16d1776c10e94e5da18f3af4c59e
Michael Kazakov <mike.kazakov@gmail.com>
2024-02-10 16:15:46 +0000

    Test

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker build --tag nc_webdav_ubuntu_2004 webdav_ubuntu_2004
+docker create --name nc_webdav_ubuntu_2004 -p 9080:80 nc_webdav_ubuntu_2004
+docker start nc_webdav_ubuntu_2004
