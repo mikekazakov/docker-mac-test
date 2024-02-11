7fd8c4fe6688a1f79db991003b43dc49a76aa79f
Michael Kazakov <mike.kazakov@gmail.com>
2024-02-11 11:56:28 +0000

    Test

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ set -e
 set -o pipefail
 
 docker build --tag nc_webdav_ubuntu_2004 webdav_ubuntu_2004
-docker create --name nc_webdav_ubuntu_2004 -p 9080:80 nc_webdav_ubuntu_2004
+docker create --name nc_webdav_ubuntu_2004 -p 127.0.0.1:9080:80 nc_webdav_ubuntu_2004
 docker start nc_webdav_ubuntu_2004
